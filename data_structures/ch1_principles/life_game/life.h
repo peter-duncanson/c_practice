aae19d96e624cecdf273a0acfa687acc97dc9183
Peter Duncanson <85356486+peter-duncanson@users.noreply.github.com>
2026-07-16 15:51:30 -0400

    Update life.h

--- data_structures/ch1_principles/life_game/life.h
+++ data_structures/ch1_principles/life_game/life.h
@@ -1,3 +1,7 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <unistd.h>
+
 #define MAXROW 20
 #define MAXCOL 60
 
@@ -5,16 +9,16 @@ typedef enum state { DEAD, ALIVE } State;
 typedef State Grid[MAXROW + 2][MAXCOL + 2];
 
 // copy updated grid (newmap) into map
-void copyMap(Grid map, Grid newmap);
+void copy_map(Grid map, Grid newmap);
 
 // will ask the user if they want to go to the next generation
-bool userSaysYes(void);
+bool generate_next(void);
 
 // initialize the grid and input initial configuration
 void init(Grid map);
 
 // counts the number of occuped cells adjacent to row, col
-int neighborCount(Grid map, int row, int col);
+int neighbor_count(Grid map, int row, int col);
 
 // outputs the map to the terminal
-void writeMap(Grid map);
+void write_map(Grid map);
