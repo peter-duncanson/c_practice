f17620e0237042ea4ed3230db1edc64e5a77ef7c
Peter Duncanson <85356486+peter-duncanson@users.noreply.github.com>
2026-07-20 16:52:57 -0400

    Update functions.h

--- modern_c/ch9_Functions/projects/functions.h
+++ modern_c/ch9_Functions/projects/functions.h
@@ -1,2 +1,20 @@
-// Project 1
-void selection_sort(int array[n]);
+// 1
+void selection_sort(int array[], int n);
+// 2
+float compute_tax(float income);
+// 3
+void generate_random_walk(char walk[10][10]);
+void print_array(char walk[10][10]);
+// 4
+void read_word(int counts[26]);
+bool equal_array(int first[26], int second[26]);
+// 5
+void create_magic_square(int n, int magic_square[][n]);
+void print_magic_square(int n, int magic_square[][n]);
+// 6
+int f(int x);
+// 7
+int power(int x, int n);
+// 8
+int roll_dice(void);
+bool play_game(void);
